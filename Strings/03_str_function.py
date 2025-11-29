3116f330ebd733a7813dab0975b335033c25317d
vishusharma <sikaridon9@gmail.com>
2025-11-29 17:37:54 +0530

    str_function

---
+++ Strings/03_str_function.py
@@ -0,0 +1,8 @@
+name ="vishu"
+print(len(name))  # Output: 5
+print(name.upper())  # Output: VISHU
+print(name.lower())  # Output: vishu
+print(name.capitalize())  # Output: Vishu
+print(name.replace("v", "V"))  # Output: Vishu
+print(name.find("shu"))  # Output: 2
+print(name.split("s"))  # Output: ['vi', 'hu']
