efeb6818d12125d62ba415a33d024d3369fbc4ef
vishusharma <sikaridon9@gmail.com>
2025-11-29 23:08:30 +0530

    tuple of python

---
+++ LIST/03_tuple.py
@@ -0,0 +1,7 @@
+a = (1,2,3,4,5)
+print(a)
+print(type(a))
+# This code creates a tuple 'a' with five integer elements and prints the tuple along with its type.
+# Output:
+# (1, 2, 3, 4, 5)
+# <class 'tuple'>
