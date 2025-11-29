a31ed3a961b8a3cc3a3644cb59db55971e8fb80e
vishusharma <sikaridon9@gmail.com>
2025-11-29 17:16:44 +0530

    input

---
+++ Variables&Datatypes/05_types.py
@@ -0,0 +1,4 @@
+a = 56.73
+b = float(a)
+t = type(b)
+print(t)
---
+++ Variables&Datatypes/06_input.py
@@ -0,0 +1,9 @@
+a = int(input("enter number 1 : "))
+b = int(input("enter number 2 : "))
+c = int(input("enter number 3 : "))
+
+print("number a is ", a)
+print("number b is ", b)
+print("number c is ", c)
+print("sum",a+b+c
+      )
