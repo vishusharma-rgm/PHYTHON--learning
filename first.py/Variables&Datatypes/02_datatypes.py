92e51d73c9f819363392f29e16e33612b3dd8270
vishusharma <sikaridon9@gmail.com>
2025-11-29 10:00:12 +0530

    variables

---
+++ first.py/Variables&Datatypes/02_datatypes.py
@@ -0,0 +1,8 @@
+a=1 # a isan integer datatype
+b=2.5 # b is a float datatype
+c="Hello" # c is a string datatype
+d=True # d is a boolean datatype
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
---
+++ first.py/Variables&Datatypes/03_rules_variables.py
@@ -0,0 +1,9 @@
+sammer = 1
+
+roni = 789
+ 
+a = 3444
+
+asd = 2222
+
+# @smmer = 233.a # not valid cauze of @ symbol
