ba7e5c895cdc192944f292b360bcb0eed38354c6
vishusharma <sikaridon9@gmail.com>
2025-11-30 19:37:33 +0530

    contitional expression

---
+++ Conditional Expression/01_conditional.py
@@ -0,0 +1,9 @@
+a = int(input("Enter your age: " ))
+
+if(a>18):
+    print("you are above the age")
+    print("good for you")
+
+else:
+    print("you are not")
+    
---
+++ Conditional Expression/02_if_elif_else_ladder.py
@@ -0,0 +1,22 @@
+a = int(input("enter your age :"))
+
+# If elif else ladder
+if(a>=18):
+    print("you are above the age :")
+    print("good for you")
+    
+elif(a<0):
+    print("you are entering the invalid age :")
+
+elif(a==0):
+    print("You are entering 0 which is not a valid age")
+    
+else:
+    print("you are chota bachaa :")
+
+    print("end of program :")
+    
+    
+
+    
+
