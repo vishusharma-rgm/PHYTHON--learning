d80291da6751c3fc8a94850c81da0fcfde208c84
vishusharma <sikaridon9@gmail.com>
2025-11-30 19:43:32 +0530

    multiple if statements

---
+++ Conditional Expression/03_mutliple_if_statement.py
@@ -0,0 +1,23 @@
+a = int(input("enter your age :"))
+
+# if statement no -1
+if(a%2== 0):
+    print("a is even :")
+    # end of if statement no -1
+    
+#  if statement no-2
+if(a>=18):
+    print("you are above the age :")
+    print("good for you")
+    
+elif(a<0):
+    print("you are entering the invalid age :")
+    # end of if statement no -2
+
+# elif(a==0):
+#     print("You are entering 0 which is not a valid age")
+    
+# else:
+#     print("you are chota bachaa :")
+
+    print("end of program ")
