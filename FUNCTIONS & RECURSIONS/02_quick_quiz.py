b8008ee369a1220ef6898a66999ed5f939d95df9
vishusharma <sikaridon9@gmail.com>
2025-12-01 12:18:01 +0530

    quick quiz

--- FUNCTIONS & RECURSIONS/02_quick_quiz.py
+++ FUNCTIONS & RECURSIONS/02_quick_quiz.py
@@ -1,6 +1,4 @@
 # print good day
-def goodday(name, ending):
-    print("goodday," + name)
-    print(ending)
-
-goodday("vishu", "thaynku")
+def goodday(name):
+    print("goodday,")
+goodday("vishu")
---
+++ FUNCTIONS & RECURSIONS/03_function with argument.py
@@ -0,0 +1,5 @@
+def goodday(name, ending):
+    print("goodday," + name)
+    print(ending)
+
+goodday("vishu", "thaynku")
