0428e6914cfde2a6e3e515bdd80a5564761acfab
vishusharma <sikaridon9@gmail.com>
2025-12-17 00:03:14 +0530

    01_class

--- OOPS/01_class.py
+++ OOPS/01_class.py
@@ -1,14 +1,14 @@
-class employe:
+class Employee:
     company = "Google"
     name = "unknown"
     salary = 0
     location = "Earth"
-    
-    vishu = employe()
-    print(vishu.company )  # Output: Google
-    print(vishu.name )     # Output: unknown
-    print(vishu.salary )   # Output: 0
-    print(vishu.location ) # Output: Earth  
+
+vishu = Employee()
+print(vishu.company )  # Output: Google
+print(vishu.name )     # Output: unknown
+print(vishu.salary )   # Output: 0
+print(vishu.location ) # Output: Earth  
     
     
 
